3885ab8b253b6d6974d7b48fd5b0117b5511da33
hyb <76788424@qq.com>
2019-03-12 16:34:18 +0800

    modify example_test

--- example_test.go
+++ example_test.go
@@ -10,22 +10,18 @@ var (
 	nonExistsFile = pwd + "/non_exists.file"
 )
 
-func setLogger() {
-	epicker.SetLogger(os.Stderr, "", 0)
-}
-
 func ExamplePrint() {
+	epicker.SetLogger(os.Stdout, "", 0)
 	_, err := os.Open(nonExistsFile)
 	epicker.Print(err)
-	// Todo: something
 	// Output:
 	// open /usr/local/var/go/src/github.com/goindow/epicker/non_exists.file: no such file or directory
 }
 
 func ExamplePrintf() {
+	epicker.SetLogger(os.Stdout, "", 0)
 	_, err := os.Open(nonExistsFile)
 	epicker.Printf(err, "format %s", "custom erorr info")
-	// Todo: something
 	// Output:
-	// format custom error info (open /usr/local/var/go/src/github.com/goindow/epicker/non_exists.file: no such file or directory)
+	// format custom erorr info (open /usr/local/var/go/src/github.com/goindow/epicker/non_exists.file: no such file or directory)
 }
